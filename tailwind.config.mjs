4b1ec0fc0b2b43d7643580b0b0d0f0d74c6545b8
Char <143526767+char667v@users.noreply.github.com>
2024-10-27 23:27:08 +0100

    prøver at fixe liquid i btn

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,7 +14,7 @@ export default {
 		},
 		extend: {
 			fontFamily: {
-				poppins: ['Poppins', 'sans-serif'],
+				/* poppins: ['Poppins', 'sans-serif'], */
 				serif: ['Lora', 'serif'],
 			},
 			spacing: {
